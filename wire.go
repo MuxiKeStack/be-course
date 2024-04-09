4e7e9f9d556c75faaf4e503300176597fc7e981b
bigdust <3264085417@qq.com>
2024-04-10 01:51:24 +0800

    wire

---
+++ wire.go
@@ -0,0 +1,38 @@
+//go:build wireinject
+
+package main
+
+import (
+	"github.com/MuxiKeStack/be-course/event"
+	"github.com/MuxiKeStack/be-course/grpc"
+	"github.com/MuxiKeStack/be-course/ioc"
+	"github.com/MuxiKeStack/be-course/repository"
+	"github.com/MuxiKeStack/be-course/repository/cache"
+	"github.com/MuxiKeStack/be-course/repository/dao"
+	"github.com/google/wire"
+)
+
+func InitApp() *App {
+	wire.Build(
+		wire.Struct(new(App), "*"),
+		//consumer
+		ioc.InitConsumers,
+		event.NewCourseListEventConsumer,
+		// grpc
+		ioc.InitGRPCxKratosServer,
+		grpc.NewCourseServiceServer,
+		ioc.InitPerformanceFallBackCourseService,
+		ioc.InitProducer,
+		ioc.InitKafka,
+		repository.NewCachedCourseRepository, repository.NewCourseSubscriptionRepository,
+		cache.NewRedisCourseCache,
+		dao.NewGORMCourseDAO, dao.NewGORMCourseSubscriptionDAO,
+		ioc.InitCCNUClient,
+		// 第三方组件
+		ioc.InitRedis,
+		ioc.InitEtcdClient,
+		ioc.InitDB,
+		ioc.InitLogger,
+	)
+	return &App{}
+}
