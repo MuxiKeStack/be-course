6cf307f04aac8a3bd042ff0a2bc53ecd582e2690
bigdust <3264085417@qq.com>
2024-04-10 01:45:34 +0800

    feat: 添加当前学年期判断，合理调用ccnu

--- service/course.go
+++ service/course.go
@@ -19,12 +19,19 @@ type CourseService interface {
 }
 
 type courseService struct {
-	ccnu ccnuv1.CCNUServiceClient
-	repo repository.CourseRepository
+	ccnu        ccnuv1.CCNUServiceClient
+	repo        repository.CourseRepository
+	currentYear string
+	currentTerm string
+}
+
+func NewCourseService(ccnu ccnuv1.CCNUServiceClient, repo repository.CourseRepository,
+	currentYear string, currentTerm string) CourseService {
+	return &courseService{ccnu: ccnu, repo: repo,
+		currentYear: currentYear, currentTerm: currentTerm}
 }
 
 func (s *courseService) FindIdOrCreateByCourse(ctx context.Context, course domain.Course) (int64, error) {
-	// TODO 这个方法预期频率高，要优化性能
 	id, err := s.repo.FindIdByCourse(ctx, course)
 	if err == nil {
 		return id, nil
@@ -44,17 +51,26 @@ func (s *courseService) FindIdOrCreateByCourse(ctx context.Context, course domai
 }
 
 func (s *courseService) List(ctx context.Context, studentId string, password string, year string, term string, uid ...int64) ([]domain.Course, error) {
-	// 教务系统查询，
+	// 从课程接口，判断是否选课中，好像都无所谓，都返回就行了，但是后面发表课评的时候要判断是否选课中
+	isHistory := year < s.currentYear || year == s.currentYear && term < s.currentTerm
+	var src ccnuv1.Source
+	// 判断学年期，从成绩接口还是老接口
+	if isHistory {
+		src = ccnuv1.Source_GradeApi
+	} else {
+		src = ccnuv1.Source_OldXkApi
+	}
+	var courses []domain.Course
 	res, err := s.ccnu.CourseList(ctx, &ccnuv1.CourseListRequest{
 		StudentId: studentId,
 		Password:  password,
 		Year:      year,
 		Term:      term,
+		Source:    src,
 	})
 	if err != nil {
 		return nil, err
 	}
-	var courses []domain.Course
 	courses = slice.Map(res.Courses, func(idx int, src *ccnuv1.Course) domain.Course {
 		return domain.Course{
 			CourseCode: src.GetCourseCode(),
@@ -65,15 +81,27 @@ func (s *courseService) List(ctx context.Context, studentId string, password str
 			Credit:     src.GetCredit(),
 		}
 	})
-	// 要在这里聚合出courseId
+
+	// 要在这里聚合出courseId，两种查询结果要采用不同的聚合手段,两个不同的聚合id的接口	[优胜劣汰]
 	var eg errgroup.Group
-	for i := range courses {
-		eg.Go(func() error {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
-			defer cancel()
-			courses[i].Id, err = s.FindIdOrCreateByCourse(ctx, courses[i])
-			return err
-		})
+	if isHistory {
+		for i := range courses {
+			eg.Go(func() error {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+				defer cancel()
+				courses[i].Id, err = s.FindIdOrUpsertByCourse(ctx, courses[i])
+				return err
+			})
+		}
+	} else {
+		for i := range courses {
+			eg.Go(func() error {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+				defer cancel()
+				courses[i].Id, err = s.FindIdOrCreateByCourse(ctx, courses[i])
+				return err
+			})
+		}
 	}
 	err = eg.Wait()
 	if err != nil {
@@ -88,4 +116,23 @@ func (s *courseService) GetDetailById(ctx context.Context, id int64) (domain.Cou
 
 func (s *courseService) GetGradesById(ctx context.Context, id int64) ([]domain.Grade, error) {
 	return s.repo.FindGradesById(ctx, id)
+}
+
+func (s *courseService) FindIdOrUpsertByCourse(ctx context.Context, course domain.Course) (int64, error) {
+	id, err := s.repo.FindIdByCourseWithoutUnknownProperty(ctx, course)
+	if err == nil {
+		return id, nil
+	}
+	if err != repository.ErrCourseNotFound {
+		// 系统错误，向上抛
+		return 0, err
+	}
+	// 没找到，创建
+	err = s.repo.Upsert(ctx, course)
+	if err != nil {
+		// 系统错误
+		return 0, err
+	}
+	// 查主库
+	return s.repo.FindIdByCourse(ctx, course)
 }
